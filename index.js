c824b2472435a89c959f61067495866759d852b2
dilnakt-bnglr <dilna.devaraj98@gmail.com>
2025-12-10 01:21:57 +0530

    Integrated forecast api

--- index.js
+++ index.js
@@ -67,30 +67,40 @@ function getWeatherByCityName(city) {
       debugger;
       console.log("Error fetching coordinates:", error);
     });
+
+    const forecastAPI=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}&units=metric`;
+    fetch(forecastAPI).then((response)=>response.json()).then((data)=>{
+        updateForecastUI(data);
+    })
 }
 
 function updateWeatherDetailsUI(weatherData) {
-  const weatherDetails = document.querySelector(".weather-details");
+    const weatherDetails = document.querySelector(".weather-details");
+    const currentWeatherIconElement = document.querySelector(".weather-icon");
   //  Update date
   const currentDay = document.querySelector(".current-date");
   currentDay.textContent = getFormattedDate(weatherData.dt);
   // Update temperature
   const temperature = document.querySelector(".current-temperature");
   temperature.textContent = `${Math.round(weatherData.main.temp)} °C`;
+
+  // Update weather icon
+    const weatherIcon=weatherData.weather[0].icon;
+    currentWeatherIconElement.src=`https://openweathermap.org/img/wn/${weatherIcon}.png`;
   // Update location
   const location = document.querySelector(".location");
   location.textContent = `${weatherData.name},${weatherData.sys.country}`;
   //  Update humidity
   const humidity = document.createElement("p");
   humidity.classList.add("current-humidity");
-  const currentHumidity = document.querySelector(".current-humidity");
-  humidity.textContent = `Humidity :${weatherData.main.humidity} %`;
+  humidity.textContent = `Humidity: ${weatherData.main.humidity} %`;
+  weatherDetails.appendChild(humidity);
   
   // Update wind speed
   const windSpeed = document.createElement("p");
   windSpeed.classList.add("wind-speed");
-  const currentWindSpeed = document.querySelector(".wind-speed");
-  windSpeed.textContent = `Wind speed : ${weatherData.wind.speed}m/s`;
+  windSpeed.textContent = `Wind speed: ${weatherData.wind.speed}m/s`;
+  weatherDetails.appendChild(windSpeed);
 }
 
 //  Get the formatted date
@@ -100,4 +110,43 @@ function getFormattedDate(dateData) {
   const day = dateObj.toLocaleDateString("en-US", { weekday: "long" });
   const month = dateObj.toLocaleString("en-US", { month: "long" });
   return `${day}, ${month} ${date}`;
+}
+
+
+function updateForecastUI(forecastData){
+    const dayForecast={};
+    forecastData.list.forEach(forecast=>{
+        const date=new Date(forecast.dt*1000).toLocaleDateString();
+            if(!dayForecast[date]){
+                dayForecast[date]=[];
+            }
+            dayForecast[date].push(forecast);
+    });
+    
+    const forecastDays=Object.keys(dayForecast);
+
+    forecastDays.forEach((day)=>{
+        const dayData=dayForecast[day];
+        const dayTemperature=dayData[0].main.temp;
+        const dayIcon=dayData[0].weather[0].icon;
+        const dayHumidity=dayData[0].main.humidity;
+        const dayWindSpeed=dayData[0].wind.speed;
+        const forecastContainer=document.getElementsByClassName("forecast-card-container")[0];
+        const forecastCard=document.createElement("div");
+        forecastCard.classList.add("forecast-card","border-2","rounded-2xl","p-2");
+        forecastCard.innerHTML=`
+          <h2 class="text-center text-black font-semibold">12-11-2025</h2>
+          <p class="text-4xl font-semibold text-center text-white my-2">${Math.round(dayTemperature)}°C</p>
+          <p class="text-black font-bold text-sm">Humidity : ${dayHumidity}%</p>
+          <p class="text-black font-bold text-sm">Wind Speed : ${dayWindSpeed} m/s</p>
+          <div>
+            <img
+              class="weather-icon m-auto"
+              src="https://openweathermap.org/img/wn/${dayIcon}.png"
+              alt="logo"
+              width="45%"
+            />
+          </div>`;
+        forecastContainer.appendChild(forecastCard);
+    })
 }
